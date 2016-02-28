41761767b3817efd36955fad2386f421314744fb
Luke Richardson <luke.richardson001@gmail.com>
2016-02-28 21:10:38 +0000

    MODE SELECT ACTIVITY: DONE DRAWER: DONE

---
+++ app/src/main/java/com/presentech/handsup/DrawerAdapter.java
@@ -0,0 +1,57 @@
+package com.presentech.handsup;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Luke on 28/02/2016.
+ * Adapter for icons and text for Drawer
+ */
+public class DrawerAdapter extends BaseAdapter{
+
+    private Context context;
+    private ArrayList<DrawerItem> DrawerItems;
+
+    public DrawerAdapter(Context context, ArrayList<DrawerItem> navDrawerItems){
+        this.context = context;
+        this.DrawerItems = navDrawerItems;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.drawer_item_layout, null);
+        }
+        ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
+        TextView textTitle = (TextView) convertView.findViewById(R.id.title);
+
+        imgIcon.setImageResource(DrawerItems.get(position).getImageResourceID());
+        textTitle.setText(DrawerItems.get(position).getString());
+        return convertView;
+    }
+
+    //BASE ADAPTER NEEDS THESE 3 METHODS
+    @Override
+    public int getCount() {
+        return DrawerItems.size();
+    }
+    @Override
+    public Object getItem(int position) {
+        return DrawerItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+}
---
+++ app/src/main/java/com/presentech/handsup/DrawerItem.java
@@ -0,0 +1,30 @@
+package com.presentech.handsup;
+
+/**
+ * Created by Luke on 28/02/2016.
+ * Drawer item Class
+ */
+public class DrawerItem {
+    private int imageResourceID;
+    private String itemString;
+
+    public DrawerItem(){}
+
+    public DrawerItem(int imageResourceID, String itemString){
+        this.imageResourceID = imageResourceID;
+        this.itemString = itemString;
+    }
+
+    public String getString(){
+        return this.itemString;
+    }
+    public int getImageResourceID(){
+        return imageResourceID;
+    }
+    public void setString(String string){
+        this.itemString = string;
+    }
+    public void setIcon(int icon){
+        this.imageResourceID = icon;
+    }
+}
