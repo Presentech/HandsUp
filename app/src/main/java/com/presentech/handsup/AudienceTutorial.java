fe1d16885ec87f7c81e23340a1ca354947ecd938
Stephen Clarke <stephenj.clarke.sc@googlemail.com>
2016-02-29 15:45:44 +0000

    tutorial mechanics completed

--- app/src/main/java/com/presentech/handsup/AudienceTutorial.java
+++ app/src/main/java/com/presentech/handsup/AudienceTutorial.java
@@ -1,11 +1,11 @@
 package com.presentech.handsup;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AudienceTutorial extends AppCompatActivity {
 
@@ -14,14 +14,34 @@ public class AudienceTutorial extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audience_tutorial);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    public void skipClicked(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void nextClicked(View view) {
+        TextView tutorialText = (TextView) findViewById(R.id.audienceText);
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        Button skipButton = (Button) findViewById(R.id.skipButton);
+        Button doneButton = (Button) findViewById(R.id.doneButton);
+        Button backButton = (Button) findViewById(R.id.backButton);
+
+        tutorialText.setText("The button has done a thing");
+        nextButton.setVisibility(View.GONE);
+        skipButton.setVisibility(View.GONE);
+        doneButton.setVisibility(View.VISIBLE);
+        backButton.setVisibility(View.VISIBLE);
+    }
+
+    public void doneClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void backClicked(View view) {
+        setContentView(R.layout.activity_audience_tutorial);
     }
 
 }
