0b011bd472c3e8d98950c599c83e756f2062ff19
Luke Richardson <luke.richardson001@gmail.com>
2016-03-05 12:20:32 +0000

    HostingWizard with Drawer

--- app/src/main/java/com/presentech/handsup/MainActivity.java
+++ app/src/main/java/com/presentech/handsup/MainActivity.java
@@ -1,20 +1,39 @@
 package com.presentech.handsup;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void goToModeSelect(View view){
-        Intent intent = new Intent(this, ModeSelectActivity.class) ;
-        startActivity(intent);
+    public void goToModeSelect(View view) {
+        Intent modeIntent = new Intent(this, ModeSelectActivity.class);
+        startActivity(modeIntent);
+    }
+
+    public void goToHostingWizard(View view) {
+        Intent hostingIntent = new Intent(this, HostingWizardActivity.class);
+        startActivity(hostingIntent);
     }
 }
