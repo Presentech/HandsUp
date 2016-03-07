2af521f254214a727ab2b2494399e60358fedca6
Lewis Tate <lt766@york.ac.uk>
2016-03-07 13:02:00 +0000

    Added check box for saving login details.

--- app/src/main/java/com/presentech/handsup/LoginScreenActivity.java
+++ app/src/main/java/com/presentech/handsup/LoginScreenActivity.java
@@ -1,26 +1,20 @@
 package com.presentech.handsup;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class LoginScreenActivity extends AppCompatActivity {
     public final static String LOGIN_MESSAGE = "com.presentech.handsup.LOGIN_MESSAGE";
@@ -29,12 +23,14 @@ public class LoginScreenActivity extends AppCompatActivity {
     public EditText emailAddress;
     public EditText password;
     public Button button_sign_in;
+    public CheckBox saveCredentials;
     public String storedEmail;
     public String storedPassword;
-    public int loginAttempts = 0;
-    public int loginAttemptsRemaining = 5;
-    public static boolean loginEnabled = true;
-    public static boolean loginCompleted = false;
+    private int loginAttempts = 0;
+    private int loginAttemptsRemaining = 5;
+    private static boolean loginEnabled = true;
+    private static boolean loginCompleted = false;
+    private static boolean saveLoginDetails = false;
     public Intent submitLoginDetails;
 
     @Override
@@ -51,49 +47,11 @@ public class LoginScreenActivity extends AppCompatActivity {
         }
     }
 
-    /*Called when the user clicks the Login button*/
-    public void loginAttempt(View view) {
-        if (!loginCompleted) {
-            if (loginEnabled) {
-                if ((emailAddress.getText().toString().equals(storedEmail) &&
-                        (password.getText().toString().equals(storedPassword)))) {
-
-                    emailAddress = (EditText) findViewById(R.id.email_addressET);
-                    String loginMessage = "Login Successful";
-                    //Clear text field after sending text
-                    emailAddress.getText().clear();
-                    password.getText().clear();
-                    loginCompleted = true;
-                    Toast.makeText(getBaseContext(), loginMessage, Toast.LENGTH_SHORT).show();
-                    startActivity(submitLoginDetails);
-
-                } else {
-                    if (loginAttempts < maxloginAttempts) {
-                        Toast.makeText(getBaseContext(), "Login Failed", Toast.LENGTH_SHORT).show();
-                        loginAttempts++;
-                        loginAttemptsRemaining = maxloginAttempts - loginAttempts;
-                        Toast.makeText(getBaseContext(), "Attempts Remaining: " +
-                                Integer.toString(loginAttemptsRemaining), Toast.LENGTH_SHORT).show();
-                    }
-                    if (loginAttemptsRemaining == 0) {
-                        loginEnabled = false;
-                        Toast.makeText(getBaseContext(), "Login Unavailable", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            } else {
-                Toast.makeText(getBaseContext(), "Login Unavailable", Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        else {
-            startActivity(submitLoginDetails);
-        }
-    }
-
     public void loginSetup() {
         emailAddress = (EditText) findViewById(R.id.email_addressET);
         password = (EditText) findViewById(R.id.passwordET);
         button_sign_in = (Button) findViewById(R.id.button_sign_in);
+        saveCredentials = (CheckBox) findViewById(R.id.saveLoginDetails);
         submitLoginDetails = new Intent(this, HostingWizardActivity.class);
 
         //Read stored usernames and passwords from userDB text file
@@ -131,6 +89,51 @@ public class LoginScreenActivity extends AppCompatActivity {
         catch (IOException e) {
             Log.e("login activity", "Can not read User Database file: " + e.toString());
         }
+    }
+
+    /*Called when the user clicks the Login button*/
+    public void loginAttempt(View view) {
+        if (!loginCompleted) {
+            if (loginEnabled) {
+                if ((emailAddress.getText().toString().equals(storedEmail) &&
+                        (password.getText().toString().equals(storedPassword)))) {
+
+                    emailAddress = (EditText) findViewById(R.id.email_addressET);
+                    String loginMessage = "Login Successful";
+                    //Clear text field after sending text
+                    emailAddress.getText().clear();
+                    password.getText().clear();
+                    if (saveLoginDetails)   {
+                        loginCompleted = true;
+                    }
+                    Toast.makeText(getBaseContext(), loginMessage, Toast.LENGTH_SHORT).show();
+                    startActivity(submitLoginDetails);
+
+                } else {
+                    if (loginAttempts < maxloginAttempts) {
+                        Toast.makeText(getBaseContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+                        loginAttempts++;
+                        loginAttemptsRemaining = maxloginAttempts - loginAttempts;
+                        Toast.makeText(getBaseContext(), "Attempts Remaining: " +
+                                Integer.toString(loginAttemptsRemaining), Toast.LENGTH_SHORT).show();
+                    }
+                    if (loginAttemptsRemaining == 0) {
+                        loginEnabled = false;
+                        Toast.makeText(getBaseContext(), "Login Unavailable", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            } else {
+                Toast.makeText(getBaseContext(), "Login Unavailable", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        else {
+            startActivity(submitLoginDetails);
+        }
+    }
+
+    public void saveLoginDetails(View view)   {
+        saveLoginDetails = !saveLoginDetails;
     }
 
 }
