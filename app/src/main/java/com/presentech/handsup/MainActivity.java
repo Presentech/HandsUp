6e6f1de3dcf20237aa0665e040c9de005675f22e
Noor <nm824@york.ac.uk>
2016-03-10 16:40:04 +0000

    Presentation Handling Activity initial push

--- app/src/main/java/com/presentech/handsup/MainActivity.java
+++ app/src/main/java/com/presentech/handsup/MainActivity.java
@@ -6,10 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
@@ -20,15 +16,11 @@ public class MainActivity extends AppCompatActivity {
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void goToModeSelect(View view) {
