941c8e69ddbf678103dd8f4975d5ff983beb06d5
Lewis Tate <lt766@york.ac.uk>
2016-05-26 14:23:32 +0100

    Added code to manifest to force landscape login screen.

--- app/src/test/java/com/presentech/handsup/aboutActivityTest.java
+++ app/src/test/java/com/presentech/handsup/aboutActivityTest.java
@@ -1,13 +1,16 @@
 package com.presentech.handsup;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import static org.junit.Assert.assertEquals;
+
 /**
- * Created by Alex on 02/05/2016.
+ * Created by Lewis on 26/05/2016.
  */
 
 @Config(constants = BuildConfig.class, sdk = 21)
@@ -18,7 +21,14 @@ public class aboutActivityTest {
     /* Method to be run before every test*/
     @Before
     public void setup() {
-        /*Robolectric method to run our Activity */
+        /*Robolectric method to run About Activity */
         activity = Robolectric.setupActivity(aboutActivity.class);
+    }
+
+    @Test
+    public void checkListVisibility(){
+        /*Check list view with activity content is visible*/
+        assertEquals(activity.findViewById(R.id.fileListView).VISIBLE,
+                activity.findViewById(R.id.fileListView).getVisibility());
     }
 }
