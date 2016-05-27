80dcc5e8f16f351b84f5940a94d6fe409484d638
Jay <jnd502@york.ac.uk>
2016-05-27 16:03:15 +0100

    test1

--- app/src/main/java/com/presentech/handsup/SettingsActivity.java
+++ app/src/main/java/com/presentech/handsup/SettingsActivity.java
@@ -27,7 +27,6 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_main);
 
-
         setupToolBar();
         bindComponents();
         init();
@@ -35,24 +34,20 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
     }
 
     private void init() {
-
         settingList = Arrays.asList(getResources().getStringArray(R.array.setting_entries));
         mAdapter = new SettingAdapter(settingList);
         lvSetting.setAdapter(mAdapter);
     }
 
     private void addListeners() {
-
         lvSetting.setOnItemClickListener(this);
     }
 
     private void bindComponents() {
-
         lvSetting = (ListView) findViewById(R.id.listView);
     }
 
     private void setupToolBar() {
-
         toolBar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolBar);
         toolBar.setNavigationIcon(null);
@@ -60,63 +55,45 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
         toolBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 finish();
             }
         });
-
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
         switch (position){
-
             case 0 :
-
                 Intent accessibility_intent = new Intent(this, SettingsAccessibilityActivity.class);
                 startActivity(accessibility_intent);
-
                 break;
 
             case 1 :
-
                 Intent account_intent = new Intent(this, SettingsAccountActivity.class);
                 startActivity(account_intent);
-
                 break;
 
             case 2 :
-
                 Intent presentation_intent = new Intent(this, SettingsPresentationFeedbackActivity.class);
                 startActivity(presentation_intent);
-
-
                 break;
 
             case 3:
-
                 Intent file_intent = new Intent(this, SettingsFileStorageActivity.class);
                 startActivity(file_intent);
-
                 break;
 
             case 4 :
-
                 Intent intent = new Intent(this, SettingsDefaultSettingActivity.class);
                 startActivity(intent);
-
                 break;
         }
-
     }
 
     class SettingAdapter extends BaseAdapter {
 
         private List<String> settingNames;
-
         SettingAdapter(List<String> settingList) {
-
             settingNames = settingList;
         }
 
@@ -137,15 +114,12 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-
             if (convertView == null) {
-
                 convertView = getLayoutInflater().inflate(R.layout.activity_settings_custom_listview, parent,false);
             }
 
             TextView tvTitle = (TextView) convertView.findViewById(R.id.lvTitle);
             tvTitle.setText(settingNames.get(position));
-
 
             return convertView;
         }
