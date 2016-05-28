176cf564fbc4d11b6917924563141cc3ef2269ca
EdwardPrentice17-21 <epp502@york.ac.uk>
2016-05-28 12:17:17 +0100

    Ed - question JSON untested. Single Question Untested. Incoming feedback incomplete.

---
+++ app/src/main/java/com/presentech/handsup/IncomingFeedbackHandler.java
@@ -0,0 +1,67 @@
+package com.presentech.handsup;
+
+import android.content.Context;
+
+import com.presentech.handsup.SingleFeedback;
+
+/**
+ * Created by epren on 27/05/2016.
+ * This is the only code that should alter counts of feedback (clearing or adding to), or call the SQL function
+ */
+public class IncomingFeedbackHandler {
+
+
+    private static final Object MUTEX = new Object();
+
+    public  IncomingFeedbackHandler(Context context) {
+        feedbackDatabaseHandler database;
+                                       //TODO What goes here??
+  database = new  feedbackDatabaseHandler(context, "name", null, 1, "FP");
+        database.deleteTable();
+    }
+
+    public void processIncomingFeedback(String textFeedbackIn, String nameOfSQLDatabase) {
+    SingleFeedback feedbackin;
+    feedbackin  =   FeedbackJSON.FeedbackJSONParse(textFeedbackIn);
+
+//Decide what to do with the feedback
+//Test to see if abc feedback
+        if (feedbackin.getABC()!=-1)
+        {
+            //Process text
+            synchronized(MUTEX) {
+                //Update any shared values relating to text.
+            }
+        }
+        //Test to see if G/M/B feedback
+        else if (feedbackin.getGOOD_MEH_BAD()!=-1)
+        {
+            //Process text
+            synchronized(MUTEX) {
+                //Update any shared values relating to text.
+            }
+        }
+        //Test to see if text feedback
+
+        if (feedbackin.getTEXT()!=null)
+        {
+            //Process text
+            synchronized(MUTEX) {
+                //Update any shared values relating to text.
+            }
+
+        }
+
+        //its none of them. This would be a good point to have a program error log
+
+        synchronized(MUTEX) {
+            //nameOfSQLDatabase;
+        }
+    }
+
+    public static void doSomethingStatic() {
+        synchronized(MUTEX) {
+            // Do something static
+        }
+    }
+}
