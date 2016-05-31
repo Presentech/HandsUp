5fa0f6e1b98461944a154bd081efc5c4b87ed84e
Amit <ar1094@york.ac.uk>
2016-05-31 01:18:38 +0100

    "No Tests were found" error
    Please check ed

--- app/src/androidTest/java/com/presentech/handsup/SingleQuestionTest.java
+++ app/src/androidTest/java/com/presentech/handsup/SingleQuestionTest.java
@@ -2,28 +2,37 @@ package com.presentech.handsup;
 
 import android.support.test.runner.AndroidJUnit4;
 
+
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.UUID;
+
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertSame;
 import static junit.framework.Assert.assertTrue;
 
 /**
-
- * Created by Amit Ramjali on 29/05/2016.
-
+ * Created by Amit on 31/05/2016.
  */
-
 @RunWith(AndroidJUnit4.class)
+
 public class SingleQuestionTest {
+
     private SingleQuestion singlequestion;
 
-    /* Method to be run before every test*/
+/* Method to be run before every test*/
 
     @Before
-
-    public void setUp() {
+    public void setup() {
 
     }
 
@@ -34,16 +43,15 @@ public class SingleQuestionTest {
         assertNotNull(singlequestion);
     }
 
-/*    @Test
+    /*    @Test
     public void simpleConstructor() {
-        singlequestion = new SingleQuestion();
+    singlequestion = new SingleQuestion();
 
-        assertTrue(singlequestion.SLIDE == -1);*//** Slide number, float is smaller than double and can hold branches to sufficient accuracy (e.g. slide 1.51 as 1.51)*//*
-        assertTrue(singlequestion.QUESTION == -1);*//** In case we have multiple sections on a slide. Might rip this out, but design is for worst-case.*//*
-        assertTrue(singlequestion.abc == false);
-        assertTrue(singlequestion.goodmehbad == false );
-        assertTrue(singlequestion.TIME_RECEIVED == -1);
+    assertTrue(singlequestion.SLIDE == -1);*//** Slide number, float is smaller than double and can hold branches to sufficient accuracy (e.g. slide 1.51 as 1.51)*//*
+    assertTrue(singlequestion.QUESTION == -1);*//** In case we have multiple sections on a slide. Might rip this out, but design is for worst-case.*//*
+    assertTrue(singlequestion.abc == false);
+    assertTrue(singlequestion.goodmehbad == false );
+    assertTrue(singlequestion.TIME_RECEIVED == -1);
     }*/
-
 
 }
--- app/src/test/java/com/presentech/handsup/AudienceTutorialTest.java
+++ app/src/test/java/com/presentech/handsup/AudienceTutorialTest.java
@@ -1,10 +1,20 @@
 package com.presentech.handsup;
 
+import android.content.Intent;
+import android.view.View;
+
 import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.internal.Shadow;
+import org.robolectric.shadows.ShadowActivity;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by Alex on 02/05/2016.
@@ -21,9 +31,97 @@ public class AudienceTutorialTest {
         activity = Robolectric.setupActivity(AudienceTutorial.class);
     }
 
-    /*Test to verify next button */
+    /*Test to verify skip button */
+    @Test
+    public void checkSkipButton(){
+        /* Simulate button press */
+        activity.findViewById(R.id.skipButton).performClick();
+
+        /*Capture intent*/
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        Intent intent = shadowActivity.getNextStartedActivity();
+
+        /*Check intent is not null*/
+        assertNotNull(intent);
+    }
+
+    @Test
+    public void checkSkipButtonIntent() {
+    /* Simulate our button being clicked */
+        activity.findViewById(R.id.skipButton).performClick();
+
+    /* Create the Intent that we would expect */
+        Intent expectedIntent = new Intent(activity, MainActivity.class);
+
+    /*Capture the Intent created by MyActivity by using Roboelectric Shadows*/
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+
+    /*Check Intent is as expected */
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+
 
     /* Test to verify back button */
+    @Test
+    public void checkBackButton() {
+        /* Simulate button press */
+        activity.findViewById(R.id.backButton).performClick();
 
-    /*Test to verify skip button */
+        /*Capture intent*/
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        View view = shadowActivity.findViewById(android.R.id.content);
+
+        //assertEquals(R.id.content, shadowActivity.getContentView().getId());
+
+        /*Check intent is not null*/
+        assertNotNull(view);
+
+    }
+
+
+
+    /*Test to verify next button */
+    @Test
+    public void checkNextButton(){
+        /* Simulate button press */
+        activity.findViewById(R.id.nextButton).performClick();
+
+        /*Capture intent*/
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        View view = shadowActivity.findViewById(android.R.id.content);
+
+        /*Check intent is not null*/
+        assertNotNull(view);
+    }
+
+    /*Test to verify done button */
+    @Test
+    public void checkDoneButton() {
+        /* Simulate button press */
+        activity.findViewById(R.id.doneButton).performClick();
+
+        /*Capture intent*/
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        Intent intent = shadowActivity.getNextStartedActivity();
+
+        /*Check intent is not null*/
+        assertNotNull(intent);
+    }
+    @Test
+    public void checkDoneButtonIntent() {
+    /* Simulate our button being clicked */
+        activity.findViewById(R.id.doneButton).performClick();
+
+    /* Create the Intent that we would expect */
+        Intent expectedIntent = new Intent(activity, AudienceSessionSelect.class);
+
+    /*Capture the Intent created by MyActivity by using Roboelectric Shadows*/
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+
+    /*Check Intent is as expected */
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+
 }
